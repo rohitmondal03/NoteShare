1bb34aef4bf833eae4fb4d75dcff25e394c5d801
Rohit <rohitmondall8000@gmail.com>
2023-11-06 20:32:51 +0530

    deployiing in production

--- src/components/fonts/font.tsx
+++ src/components/fonts/font.tsx
@@ -1,12 +1,12 @@
 import { Baloo_Bhai_2 as BalooBhai } from "next/font/google"
-import { Montserrat } from "next/font/google";
+import { Amaranth } from "next/font/google";
 
 export const balooFont = BalooBhai({
   subsets: ["latin"],
   weight: ["400", "600", "700", '800']
 })
 
-export const monsterratFont = Montserrat({
+export const monsterratFont = Amaranth({
   subsets: ["latin"],
-  weight: ["400", "700", "800", "900"]
+  weight: ["400"]
 })
